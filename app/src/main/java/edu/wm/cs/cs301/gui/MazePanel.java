9326b66e5d1bec61a05986ec67013a39a1a93c43
Evan Hellersund <ehhellersund@wm.edu>
2023-05-04 00:53:58 -0400

    Passed values from first to second state

--- app/src/main/java/edu/wm/cs/cs301/gui/MazePanel.java
+++ app/src/main/java/edu/wm/cs/cs301/gui/MazePanel.java
@@ -3,11 +3,22 @@ package edu.wm.cs.cs301.gui;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.View;
+import android.graphics.Canvas;
+import android.graphics.Bitmap;
+import android.graphics.Paint;
+
 
 import androidx.annotation.Nullable;
 
 public class MazePanel extends View {
-    public MazePanel(Context context, @Nullable AttributeSet attrs) {
-        super(context, attrs);
+
+    public MazePanel(Context context) {
+        super(context);
     }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+    }
+
 }
